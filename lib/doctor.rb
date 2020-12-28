126d5602e248f90c1aa80c631f1ea69e4ded1833
BillyMiles <williammiles201@gmail.com>
2020-12-28 16:26:21 -0500

    commit tree b189926d865cf52cb5211a3ab64e7d129c5fb538

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,40 @@
+class Doctor
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    def new_appointment(date, patient)
+      Appointment.new(date, patient, self)
+    end
+  
+    def appointments
+      Appointment.all.select { |appointment| appointment.doctor == self }
+    end
+  
+    def patients
+      appointments.map(&:patient)
+    end
+  end
 
+
+#     def new_song(name, genre)
+#       Song.new(name, self, genre)
+#     end
+  
+#     def songs
+#       Song.all.select { |song| song.artist == self }
+#     end
+  
+#     def genres
+#       songs.map(&:genre)
+#     end
+#   end
